fb5e7a07a6cdfc01b9b4b9cc05e6a0728f31731e
Chris W Jones <chris@christopherjones.us>
2024-03-04 14:19:25 -0500

    Remove duplicate stuff from proxmox config

--- hosts/common/proxmox.nix
+++ hosts/common/proxmox.nix
@@ -20,43 +20,6 @@
     ls = "ls -l";
     la = "ls -a";
   };
-  environment.systemPackages = with pkgs; [
-    bat
-    bc
-    curl
-    delta
-    dnsutils
-    fd
-    git
-    glibcLocales
-    htop
-    jq
-    just
-    magic-wormhole
-    mosh
-    ncdu
-    ncmpcpp
-    pv
-    tailscale
-    tmux
-    vim
-    wget
-  ];
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
 
   networking.useDHCP = false;
   networking = {
@@ -73,79 +36,10 @@
   };
   networking.nameservers = [ "192.168.104.1" ];
 
-  ## Garbage collection
-  # https://nixos.wiki/wiki/Storage_optimization#Automation
-  nix.gc = {
-    automatic = true;
-    dates = "Monday 01:00 America/Kentucky/Louisville";
-    options = "--delete-older-than 7d";
-  };
-
-  # Run garbage collection whenever there is less than 500MB free space left
-  nix.extraOptions = ''
-    min-free = ${toString (500 * 1024 * 1024)}
-  '';
-
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  security.pam.enableSSHAgentAuth = true;
-
   services.resolved = {
     enable = true;
     domains = [ "hilandchris.com" "vpn.hilandchris.com" ];
     fallbackDns = [ "45.90.28.65" "45.90.30.65" ];
-  };
-  services.qemuGuest.enable = true;
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
-
-  ## Unattended upgrade
-  system.autoUpgrade = {
-    enable = true;
-    allowReboot = true;
-    dates = "weekly America/Kentucky/Louisville";
-  };
-
-  ## Optional: Clear >1 month-old logs
-  systemd = {
-    services.clear-log = {
-      description = "Clear >1 month-old logs every week";
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStart = "${pkgs.systemd}/bin/journalctl --vacuum-time=30d";
-      };
-    };
-    timers.clear-log = {
-      wantedBy = [ "timers.target" ];
-      partOf = [ "clear-log.service" ];
-      timerConfig.OnCalendar = "weekly UTC";
-    };
-  };
-
-  # Set your time zone.
-  time.timeZone = "America/Kentucky/Louisville";
-
-  users = {
-    mutableUsers = false; # Disable passwd
-
-    users = {
-      root = {
-        hashedPassword = "*"; # Disable root password
-      };
-      chrisj = {
-        hashedPassword = "$y$jET$eQzDbgzetAjg3ybPY/lgn.$LQi1H9MqV47wJeZB7QB3n4J95Om3Gc.U813i4M5hX03";
-        isNormalUser = true;
-        extraGroups = [ "networkmanager" "wheel" ];
-      };
-    };
   };
 
   # This value determines the NixOS release from which the default
